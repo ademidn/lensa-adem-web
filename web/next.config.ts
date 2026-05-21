f8fedfa4414c1459dec7b578b80080eba0d1d84f
bangsuwan <angsulwan@gmail.com>
2026-05-21 11:05:19 +0700

    update next.config.ts

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ["192.168.1.2"],
 };
 
 export default nextConfig;
